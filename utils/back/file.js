2ed025a496d59ce64628784569f3334707a340d0
EvansUchwa <johnsonevans686@gmail.com>
2024-02-16 02:43:11 +0100

    Try to fix vercel cors issue part 3,ive done it try to fix folder path

--- utils/back/file.js
+++ utils/back/file.js
@@ -1,7 +1,28 @@
 import path from "path";
 import { writeFile } from "fs/promises";
 import uniqid from 'uniqid';
+const fs = require('fs');
+
+function createSubdirectoryInPublic(subdirectoryName) {
+    const publicPath = path.join(process.cwd(), 'public', subdirectoryName);
+
+    try {
+        if (!fs.existsSync(publicPath)) {
+            fs.mkdirSync(publicPath);
+            console.log(`Subdirectory '${subdirectoryName}' created in the 'public' directory.`);
+        } else {
+            console.log(`Subdirectory '${subdirectoryName}' already exists in the 'public' directory.`);
+        }
+    } catch (error) {
+        console.error('Error creating subdirectory:', error);
+    }
+}
+
 export async function fileUploadManager(file, origin, folderPath) {
+    createSubdirectoryInPublic('storage')
+    createSubdirectoryInPublic('storage/resumes')
+    createSubdirectoryInPublic('storage/profilPics')
+
     const buffer = Buffer.from(await file.arrayBuffer());
     const originName = file.name.replaceAll(" ", "_");
     const fileExtension = path.extname(originName);
@@ -17,4 +38,6 @@ export async function fileUploadManager(file, origin, folderPath) {
         buffer
     );
     return profilPicObj;
+    // return { url: 'http://localhost:3000/public/storage/profilPics/1708036088hiretop-ast-f0wlsnsl4u8.jpg' };
 }
+
