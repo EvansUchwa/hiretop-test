3052e2b41995d2a721a0f2cfc4bfebd5bcc43ace
EvansUchwa <johnsonevans686@gmail.com>
2024-02-16 01:28:12 +0100

    Try to fix vercel cors issue part 2

--- axios-config.js
+++ axios-config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api', // Remplacez par votre URL de base
+    baseURL: '/api', // Remplacez par votre URL de base
     timeout: 5000, // Définissez le timeout souhaité
 });
 
